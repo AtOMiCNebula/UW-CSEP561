c113d53cbe8b13f042417bb84e02f45578513584
Jeff Weiner <jdweiner@cs.washington.edu>
2013-11-15 07:51:29 -0800

    Update p1 references to p2 references
    And remove p1-specific code.  Keep all the boilerplate code in place though,
    for 100% more synergy!

--- p2/LoadBalancingSwitch.py
+++ p2/LoadBalancingSwitch.py
@@ -1,6 +1,6 @@
 """
-CSE P 561 Network Systems - Project 1 (Learning Switch)
-October 28th, 2013
+CSE P 561 Network Systems - Project 2 (Load-Balancing Switch)
+November 18th, 2013
 Jeff Weiner <jdweiner@cs.washington.edu>
 """
 
@@ -14,23 +14,17 @@ log = core.getLogger()
 
 HARD_TIMEOUT = 30
 IDLE_TIMEOUT = 30
-class LearningSwitch (EventMixin):
+class LoadBalancingSwitch (EventMixin):
 
   def __init__ (self,connection):
-    # Switch we'll be adding L2 learning switch capabilities to
+    # Switch we'll be adding L2 load-balancing switch capabilities to
     self.connection= connection
     self.listenTo(connection)
-    self.mactable = {}
 
   def _handle_PacketIn (self, event):
 
     # parsing the input packet
     packet = event.parse()
-    
-    # updating out mac to port mapping
-    if packet.src not in self.mactable or self.mactable[packet.src] != event.ofp.in_port:
-        self.mactable[packet.src] = event.ofp.in_port
-        log.debug("Learned port %s connects to %s" % (event.ofp.in_port, packet.src))
     
     if packet.type == packet.LLDP_TYPE or packet.type == 0x86DD:
       # Drop LLDP packets 
@@ -42,23 +36,6 @@ class LearningSwitch (EventMixin):
       msg.in_port = event.port
       self.connection.send(msg)
       return
-    elif packet.dst in self.mactable:
-      # If we know the destination, but are being told about it, recreate the
-      # flow, since this is either the initial discovery, or the flow expired.
-      log.debug("Establishing flow to deliver packets for %s to port %s" % (packet.dst, self.mactable[packet.dst]))
-      fm = of.ofp_flow_mod()
-      fm.idle_timeout = IDLE_TIMEOUT
-      fm.hard_timeout = HARD_TIMEOUT
-      fm.actions.append(of.ofp_action_output(port=self.mactable[packet.dst]))
-
-      # Defining the match via from_packet will cause us to establish a flow
-      # for each unique packet type per destination, instead of just per dest.
-      fm.match = of.ofp_match(dl_dst=packet.dst, dl_src=packet.src)
-      #fm.match = of.ofp_match.from_packet(packet)
-
-      fm.data = event.ofp   # Deliver the packet along this flow
-      self.connection.send(fm)
-      return
 
     log.debug("Port for %s unknown -- flooding" % (packet.dst,))
     msg = of.ofp_packet_out()
@@ -67,17 +44,17 @@ class LearningSwitch (EventMixin):
     msg.in_port = event.port
     self.connection.send(msg)
 
-class learning_switch (EventMixin):
+class load_balancing_switch (EventMixin):
 
   def __init__(self):
     self.listenTo(core.openflow)
 
   def _handle_ConnectionUp (self, event):
     log.debug("Connection %s" % (event.connection,))
-    LearningSwitch(event.connection)
+    LoadBalancingSwitch(event.connection)
 
 
 def launch ():
-  #Starts an L2 learning switch.
-  core.registerNew(learning_switch)
+  #Starts an L2 load-balancing switch.
+  core.registerNew(load_balancing_switch)
 
