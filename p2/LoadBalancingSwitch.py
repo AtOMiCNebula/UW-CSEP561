d9f9b77e54f891ea7e6bc4ee9523b0e3eba0da1f
Jeff Weiner <jdweiner@cs.washington.edu>
2013-11-16 08:53:40 -0800

    Add logic to allow switches to "own" hosts
    This will allow hosts to determine whether they are forwarding to another
    switch, or an endpoint (which will need eventual packet rewriting).

--- p2/LoadBalancingSwitch.py
+++ p2/LoadBalancingSwitch.py
@@ -4,6 +4,7 @@ November 18th, 2013
 Jeff Weiner <jdweiner@cs.washington.edu>
 """
 
+from datetime import *
 from pox.core import core
 import pox.openflow.libopenflow_01 as of
 from pox.lib.revent import *
@@ -16,16 +17,26 @@ HARD_TIMEOUT = 30
 IDLE_TIMEOUT = 30
 class LoadBalancingSwitch (EventMixin):
 
-  def __init__ (self,connection):
+  def __init__ (self,connection,hostlocations):
     # Switch we'll be adding L2 load-balancing switch capabilities to
     self.connection= connection
     self.listenTo(connection)
+    self.hostlocations = hostlocations
 
   def _handle_PacketIn (self, event):
 
     # parsing the input packet
     packet = event.parse()
     
+    # If no switch has ever seen this host before, then it must be directly
+    # connected to us!  Record that in our global list.
+    if packet.src not in self.hostlocations or self.hostlocations[packet.src]["lastseen"] < (datetime.now()-timedelta(0,IDLE_TIMEOUT)):
+      #log.debug("s%s takes ownership of host %s" % (self.connection.ID, packet.src))
+      self.hostlocations[packet.src] = { "ID": self.connection.ID, "lastseen": datetime.now() }
+    elif self.hostlocations[packet.src]["ID"] == self.connection.ID:
+      #log.debug("s%s continues to own host %s" % (datetime.now(), self.connection.ID, packet.src))
+      self.hostlocations[packet.src]["lastseen"] = datetime.now()
+
     if packet.type == packet.LLDP_TYPE or packet.type == 0x86DD:
       # Drop LLDP packets 
       # Drop IPv6 packets
@@ -48,10 +59,11 @@ class load_balancing_switch (EventMixin):
 
   def __init__(self):
     self.listenTo(core.openflow)
+    self.hostlocations = {}
 
   def _handle_ConnectionUp (self, event):
     log.debug("Connection %s" % (event.connection,))
-    LoadBalancingSwitch(event.connection)
+    LoadBalancingSwitch(event.connection, self.hostlocations)
 
 
 def launch ():
