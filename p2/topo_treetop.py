a0dee39904373b2148ceccaf1c23f52c5a33e0ff
Jeff Weiner <jdweiner@cs.washington.edu>
2013-11-18 03:23:12 -0800

    Convert Tree topo to TreeTop topo
    Now there's a h0 that connects to the root switch, which allows for more
    accurate representation of how external traffic would enter this network.

--- p2/topo_treetop.py
+++ p2/topo_treetop.py
@@ -1,13 +1,20 @@
-"Library of potentially useful topologies for Mininet"
+"""
+CSE P 561 Network Systems - Project 2 (Load-Balancing Switch)
+November 18th, 2013
+Jeff Weiner <jdweiner@cs.washington.edu>
+
+This is a slightly adapted version of Mininet's Tree topo class, to allow for
+a host to be attached to the root of the tree, which better represents how
+external traffic would enter my ideal load-balanced network.
+"""
 
 from mininet.topo import Topo
-from mininet.net import Mininet
 
-class TreeTopo( Topo ):
+class TreeTopTopo( Topo ):
     "Topology for a tree network with a given depth and fanout."
 
     def __init__( self, depth=1, fanout=2 ):
-        super( TreeTopo, self ).__init__()
+        super( TreeTopTopo, self ).__init__()
         # Numbering:  h1..N, s1..M
         self.hostNum = 1
         self.switchNum = 1
@@ -20,6 +27,9 @@ class TreeTopo( Topo ):
         isSwitch = depth > 0
         if isSwitch:
             node = self.addSwitch( 's%s' % self.switchNum )
+            if self.switchNum == 1:
+                c = self.addHost( 'h0' )
+                self.addLink( node, c )
             self.switchNum += 1
             for _ in range( fanout ):
                 child = self.addTree( depth - 1, fanout )
@@ -29,8 +39,4 @@ class TreeTopo( Topo ):
             self.hostNum += 1
         return node
 
-
-def TreeNet( depth=1, fanout=2, **kwargs ):
-    "Convenience function for creating tree networks."
-    topo = TreeTopo( depth, fanout )
-    return Mininet( topo, **kwargs )
+topos = { 'treetop': TreeTopTopo }
